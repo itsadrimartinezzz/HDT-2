ba92d759f01882a9ab95f75c75ec8c31f0cd6ebc
Adriana Martínez Fuentes <mar24086@uvg.edu.gt>
2025-01-30 08:53:31 -0600

    Update Stack.java

--- HT-2/Stack.java
+++ HT-2/Stack.java
@@ -1,23 +1,50 @@
-public interface Stack<E> 
-{
+import java.util.Vector;
 
-   public void push(E item);
-   // pre: 
-   // post: item is added to stack
-   // will be popped next if no intervening push
-   
-   public E pop();
-   // pre: stack is not empty
-   // post: most recently pushed item is removed and returned
-   
-   public E peek();
-   // pre: stack is not empty
-   // post: top value (next to be popped) is returned
-   
-   public boolean empty();
-   // post: returns true if and only if the stack is empty
-   
-   public int size();
-   // post: returns the number of elements in the stack
+// Definición de la interfaz Stack<E> si no existe
+interface Stack<E> {
+    void push(E item);
+    E pop();
+    E peek();
+    boolean isEmpty();
+    int size();
+}
 
+// Implementación de la pila con Vector
+public class VectorStack<E> implements Stack<E> {
+    private Vector<E> stack;
+
+    public VectorStack() {
+        stack = new Vector<>();
+    }
+
+    @Override
+    public void push(E item) {
+        stack.add(item);
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack vacío");
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack vacío");
+        }
+        return stack.get(stack.size() - 1);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return stack.size();
+    }
 }
