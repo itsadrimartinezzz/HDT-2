9a8f17a6ef5eaa11f656e0ced927ba31c2c2c5f4
Adriana Martínez Fuentes <mar24086@uvg.edu.gt>
2025-01-30 08:18:57 -0600

    Update ReadFile.java

--- HT-2/Hoja 2/ReadFile.java
+++ HT-2/Hoja 2/ReadFile.java
@@ -1,31 +1,38 @@
-import javax.management.RuntimeErrorException;
 import java.io.*;
 import java.io.BufferedReader;
+
 public class ReadFile {
 
     public String LeerArchivo(String ruta){//al llamar el metodo colocar la ruta del archivo
         File archivo;
         FileReader fr;
-        BufferedReader br;
+        BufferedReader br = null;
         String linea;
+        String contenido = "";
 
         try{
             archivo = new File(ruta); //abrimos el archivo
             fr = new FileReader(archivo); //lectura del archivo
             br = new BufferedReader(fr); //guardar en memoria lo que vamos a leer
 
-
             while((linea = br.readLine()) != null){ //vamos recorriendo por lineas de codigo y captura el salto
-                return linea;
-
+                contenido += linea + "\n";
             }
+
             br.close();
             fr.close();
 
-
-        }   catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                if(br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-        return "";
+        return contenido;
     }
 }
